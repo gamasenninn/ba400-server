11b1489a8d8a730d1c0b289ff2a34a9fd7374c22
gamasenninn <gamasenninn@yahoo.co.jp>
2022-09-16 16:59:37 +0900

    ステータス応答関連実装

--- app/main.py
+++ app/main.py
@@ -11,9 +11,12 @@ CORS(app, support_credentials=True)
 def test():
     return app.send_static_file('test-tpcl.html')
 
+
+
+
 @app.route('/tpclmaker',methods=["GET","POST"])
 @cross_origin(supports_credentials=True)
-def makepdf():
+def maketpcl():
 
     if request.method == "GET":
         return redirect('/tpclmaker/print_conf_ip')
@@ -28,7 +31,7 @@ def makepdf():
 
 @app.route('/tpclmaker/<jsonc_file>')
 @cross_origin(supports_credentials=True)
-def makepdf_file(jsonc_file):
+def maketpcl_file(jsonc_file):
 
     conf = tpcl.read_jsonc_file(jsonc_file+".jsonc")
     ret = tpcl.tpcl_maker(conf)
@@ -36,6 +39,22 @@ def makepdf_file(jsonc_file):
         with open('tpcl_send.log','r',encoding='utf-8') as f:
             response = f.read()
             return f"<pre>{response}</pre>"
+
+@app.route('/tpclmaker/status/<jsonc_file>')
+@cross_origin(supports_credentials=True)
+def get_status(jsonc_file):
+
+    #return jsonc_file
+    conf = tpcl.read_jsonc_file(jsonc_file+".jsonc")
+    data =  tpcl.analize_status(conf)
+    return json.dumps(data, indent=2, ensure_ascii=False)
+
+@app.route('/tpclmaker/status',methods=["POST"])
+@cross_origin(supports_credentials=True)
+def get_status_post():
+        d = request.json
+        data =  tpcl.analize_status(d)
+        return json.dumps(data, indent=2, ensure_ascii=False)
 
 if __name__ == '__main__':
 
